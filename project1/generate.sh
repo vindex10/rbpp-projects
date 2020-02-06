6f1002fbe90b5c9cf0f07dec4b22ee0f407e0918
Victor Ananyev <vindex10@gmail.com>
2020-02-07 00:31:39 +0100

    tee reads in batches. not entire output being offloaded

--- project1/generate.sh
+++ project1/generate.sh
@@ -13,7 +13,8 @@ FORMCMD='form -q -p ../external/form-square/:symbolic/'
 mkdir -p "$OUTPUT_DIR"
 
 MELSQ_FILE="$OUTPUT_DIR/melsq.output"
-MELSQ_NAME=$($FORMCMD symbolic/melsq.frm | tee "$MELSQ_FILE" | extract_res_name)
+$FORMCMD symbolic/melsq.frm > "$MELSQ_FILE"
+MELSQ_NAME=$(cat "$MELSQ_FILE" | extract_res_name)
 
 $FORMCMD -D INPUTFILE="$MELSQ_FILE" -D FROMNAME="$MELSQ_NAME" symbolic/dcrsc.frm > "$OUTPUT_DIR/dcrsc.output"
 $FORMCMD -D INPUTFILE="$MELSQ_FILE" -D FROMNAME="$MELSQ_NAME" symbolic/totcrsc.frm > "$OUTPUT_DIR/totcrsc.output"
