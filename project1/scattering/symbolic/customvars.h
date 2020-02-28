e91ab95bf2d028019cfad02dea58157dfee5caca
Victor Ananyev <vindex10@gmail.com>
2020-02-28 12:10:32 +0100

    add pages

--- project1/scattering/symbolic/customvars.h
+++
@@ -1,8 +0,0 @@
-Symbol e, mB, mMu, mZ, mH, gW, gZ, mW, sW, wZ, wH, Qb;
-Symbol s, t;
-Symbol PI;
-CFunction SQRT;
-Symbol [photon prop];
-Symbol [higgs prop];
-Symbol [z prop];
-Symbol [gW/mW];
--- project1/scattering/symbolic/param.h
+++
@@ -1,3 +0,0 @@
-#define DEBUG "0"
-#define LINES "2"
-#define LORENTZIDXNUM "10"
